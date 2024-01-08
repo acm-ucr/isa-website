ed18f7d3b6bc18e19f3b84b77dd1302795d02291
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-01-07 19:12:03 -0800

    tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,16 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
-    colors: {
-      isa: {
-        black: "#303030",
-        white: "#FFFFFF",
-        "blue-100": "#8CD8FA",
-        "blue-200": "#06A0DB",
-        "yellow-100": "#FFDA7C",
-        "yellow-200": "#FFA824",
+    extend: {
+      colors: {
+        isa: {
+          black: "#303030",
+          white: "#FFFFFF",
+          "blue-100": "#8CD8FA",
+          "blue-200": "#06A0DB",
+          "yellow-100": "#FFDA7C",
+          "yellow-200": "#FFA824",
+        },
       },
     },
   },
