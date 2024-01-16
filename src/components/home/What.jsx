c0a22c1e16cca23dc5c46fadfc83804621c9da5b
Shan <ssant096@ucr.edu>
2024-01-15 22:31:30 -0800

    added what component

---
+++ src/components/home/What.jsx
@@ -0,0 +1,20 @@
+import Title from "@/components/Title";
+import Button from "@/components/Button";
+
+const What = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Title text="What We Do" />
+      <p class="text-center text-xl mx-10 my-5">
+        Through this organization we try to promote the positive interaction
+        among students of either Indian origin, or desirous of understanding the
+        Indian culture. This includes ensuring the smooth transition of
+        newly-arrived graduate students from India into the social setup of the
+        university campus.
+      </p>
+      <Button text={"CHECK OUT NEW STUDENTS HANDBOOK"} link={"handbook"} />
+    </div>
+  );
+};
+
+export default What;
