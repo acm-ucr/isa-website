480e3238037e459b458fb57fbe48c341404c3ba1
Shan <ssant096@ucr.edu>
2024-01-16 22:35:11 -0800

    Used more suitable formating for what component

--- src/components/home/What.jsx
+++ src/components/home/What.jsx
@@ -3,16 +3,16 @@ import Button from "@/components/Button";
 
 const What = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center">
       <Title text="What We Do" />
-      <p class="text-center text-xl mx-10 my-5">
+      <p class="text-center text-xl my-3">
         Through this organization we try to promote the positive interaction
         among students of either Indian origin, or desirous of understanding the
         Indian culture. This includes ensuring the smooth transition of
         newly-arrived graduate students from India into the social setup of the
         university campus.
       </p>
-      <Button text={"CHECK OUT NEW STUDENTS HANDBOOK"} link={"handbook"} />
+      <Button text="CHECK OUT NEW STUDENTS HANDBOOK" link="handbook" />
     </div>
   );
 };
