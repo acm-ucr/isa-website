67fa32ba5c34fbfc8296fcf48c105b8a1830833a
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-01-19 21:38:39 -0800

    Revert "created the About page"
    This reverts commit 5ef7f97473e6bc43aaae52f2ded8e1bb402d93a9.

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,23 +1,5 @@
-"use client";
-import Logo from "../../../public/assets/logo.svg";
-import Image from "next/image";
-import { sections } from "@/data/aboutSections/sections.js";
-
 const About = () => {
-  return (
-    <div>
-      <Image src={Logo} className="py-8 mx-auto h-[400px] w-[400px]" />
-      {sections.map((section, index) => (
-        <div
-          key={index}
-          className="flex flex-col mx-auto w-1/2 text-center pb-12"
-        >
-          <div className="pb-6">{section.title}</div>
-          <div className="text-2xl">{section.description}</div>
-        </div>
-      ))}
-    </div>
-  );
+  return <>About</>;
 };
 
 export default About;
