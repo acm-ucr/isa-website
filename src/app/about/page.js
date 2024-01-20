61731a40558a9d7938e98806fcc328bc4ddfcecc
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-01-20 09:03:04 -0800

    fixed build error in About page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Logo from "../../../public/assets/logo.svg";
 import Image from "next/image";
-import { sections } from "@/data/aboutSections/sections.js";
+import { sections } from "@/data/aboutSections/Sections.js";
 
 const About = () => {
   return (
