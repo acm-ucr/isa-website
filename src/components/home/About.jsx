a687fd53835af21faeddfe224f7f2ac0b3119745
Zami Alam <zalam007@ucr.edu>
2024-01-22 19:47:46 -0800

    Essentially completed About Componenet

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -2,23 +2,28 @@ import React from "react";
 import Title from "@/components/Title";
 
 import Image from "next/image";
-import { navlogo } from "@/public/assets/navlogo.svg";
+import cloud from "@/public/assets/cloud.svg";
+import navlogo from "@/public/images/navlogo.webp";
 
 const About = () => {
   return (
-    <div className="mt-10">
-      <Image navlogo={navlogo} alt="Picture of navlogo" />
-      <Title title="About us" />
+    <div className="flex flex-col items-center">
+      <div className="flex items-center px-32 z-1">
+        <Image src={navlogo} alt="Picture of navlogo" />
 
-      <div className="flex-col">
-        <p>
-          Indian Student Association-ISA at UCR serves the purpose to provide a
-          commonplace for people to share and exchange Indian culture of all
-          regions in India. We strive to provide a place for all Indians and
-          non-Indians to come to socialize, celebrate festivals, enjoy food,
-          meet new people, and learn more about Indian culture
-        </p>
+        <div className="flex-col">
+          <Title text="About us" />
+          <div className="text-center w-1/2 mx-auto mt-10">
+            Indian Student Association-ISA at UCR serves the purpose to provide
+            a commonplace for people to share and exchange Indian culture of all
+            regions in India. We strive to provide a place for all Indians and
+            non-Indians to come to socialize, celebrate festivals, enjoy food,
+            meet new people, and learn more about Indian culture
+          </div>
+        </div>
       </div>
+
+      <Image src={cloud} alt="Picture of cloud" className="mt-[-58px] z-0" />
     </div>
   );
 };
