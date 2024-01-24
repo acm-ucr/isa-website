f4f5cda81049f8b66aa5380108f92b35424208ae
Marco Navarro <duvalincus@gmail.com>
2024-01-23 20:22:46 -0800

    fixed off-screen positioning

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Landing from "@/components/home/Landing";
+import About from "./about/page";
 
 const Home = () => {
   return (
     <>
       <Landing />
+      <About />
     </>
   );
 };
