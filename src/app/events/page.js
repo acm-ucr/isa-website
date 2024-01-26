618dc341878e3789d6a44178b4b409a6eacb7c07
achen409 <andrewr.chen@gmail.com>
2024-01-26 14:45:18 -0800

    Added title and suns

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,7 +1,28 @@
+import Title from "@/components/Title";
 import CalendarEvents from "@/components/events/Calendar";
+import Sun from "@/public/assets/sun.svg";
+import Image from "next/image";
 
 const Events = () => {
-  return <CalendarEvents />;
+  return (
+    <div className="overflow-hidden">
+      <div className="h-10" />
+      <Title text="Calendar" />
+      <CalendarEvents />
+      <div className="w-full relative flex flex-row justify-center items-center">
+        <Image
+          src={Sun}
+          alt="Blue sun"
+          className="hidden lg:block absolute lg:-right-[35%] bottom-0 scale-75"
+        />
+        <Image
+          src={Sun}
+          alt="Blue sun"
+          className="hidden lg:block absolute lg:-left-[25%] scale-75"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Events;
