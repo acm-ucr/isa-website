203a5f5974c71e7c46015c26ef688669d53a8de0
Byron Bhuiyan <rafidb8@gmail.com>
2024-01-26 03:47:27 -0800

    Board is now mobile responsive

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -9,9 +9,9 @@ const Meet = () => {
   }));
 
   return (
-    <div className="flex flex-col items-center justify-start min-h-screen pt-16">
+    <div className="flex flex-col items-center justify-start min-h-screen pt-4 sm:pt-16 md:pt-32 lg:pt-16">
       <Title text="Meet the Board" />
-      <div className="grid grid-cols-4 gap-y-12 gap-x-32 mt-10">
+      <div className="grid grid-cols-2 gap-x-6 gap-y-12 mt-4 mb-4 md:grid-cols-3 md:gap-x-16 md:mt-10 lg:grid-cols-4 lg:gap-x-32 lg:mt-10">
         {boardMembers.map((member, index) => (
           <Board key={index} name={member.name} position={member.position} />
         ))}
