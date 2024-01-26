6a9e3d151d4c3243d9d2ab5f56762eb6d093eec1
Parth Awade <parth.awade@icloud.com>
2024-01-26 14:21:05 -0800

    fixed some things

--- src/app/handbook/page.js
+++ src/app/handbook/page.js
@@ -1,12 +1,12 @@
 import Title from "@/components/Title";
 import Subtitle from "@/components/handbook/Subtitle";
+import Link from "next/link";
 
 const Handbook = () => {
   return (
-    <div className="flex flex-col items-center justify-center p-[4%]">
+    <div className="flex flex-col text-xl items-center justify-center p-[4%]">
       <Title text="New Student Handbook" />
-
-      <div className="w-2/5">
+      <div className="w-2/3">
         <Subtitle text="Arriving at UCR" />
         You can reach the US one month before the date given on your I-20 that
         is the latest you&apos;re required to show up. Ideally, you should reach
@@ -15,14 +15,14 @@ const Handbook = () => {
         Riverside, where flights from India land. Riverside is about 60-70 miles
         from there. The best way to reach Riverside from LAX is by shuttle. The
         most reliable (but might not be the cheapest) shuttle service is{" "}
-        <a
+        <Link
           href="https://www.supershuttle.com/"
           rel="noopener noreferrer"
           target="_blank"
           className="font-bold text-isa-blue-200 underline dark:text-isa-blue-200 hover:no-underline"
         >
           Super Shuttle
-        </a>
+        </Link>
         . You can look up other services on Google. Normal rates from LAX to
         Riverside are $50-75. You&apos;d need an address to reach to while
         registering the shuttle (you can also get one from outside the airport
@@ -31,7 +31,7 @@ const Handbook = () => {
         Park Apartments, chances are that you&apos;re most likely to be put up
         at either of the two for temporary accomodation. The addresses for those
         places are 160 W Big Springs Rd, Riverside, CA 92507 and 3131 Watkins
-        Dr, Riversie, CA 92507 respectively.
+        Dr, Riverside, CA 92507 respectively.
         <Subtitle text="Medical Tests" />
         UCR requires incoming students from India to only take the Tuberculosis
         test. MMR is strictly optional, unless you&apos;re under 18. You can
@@ -39,14 +39,14 @@ const Handbook = () => {
         miserly amount); it doesn&apos;t really matter. Make sure whichever is
         the case you do submit your medical forms before the deadline specified
         by the{" "}
-        <a
+        <Link
           href="https://studenthealth.ucr.edu/"
           rel="noopener noreferrer"
           target="_blank"
           className="font-bold text-isa-blue-200 underline dark:text-isa-blue-200 hover:no-underline"
         >
           Campus Health Center
-        </a>
+        </Link>
         , or there might be unnecessary hassle while registering for classes.
         <Subtitle text="Insurance" />
         Most people who get into UCR get fellowships, which would mean that
@@ -61,14 +61,14 @@ const Handbook = () => {
         However, insurance in this case is an issue. Most students can get a
         California driver&apos;s license within a matter of months anyways from
         the{" "}
-        <a
+        <Link
           href="https://www.dmv.ca.gov/portal//"
           rel="noopener noreferrer"
           target="_blank"
           className="font-bold text-isa-blue-200 underline dark:text-isa-blue-200 hover:no-underline"
         >
           DMV
-        </a>
+        </Link>
         , if they are able to get a social security number (you can get one if
         you&apos;re working on campus - includes TA/GSR).
         <Subtitle text="Fellowships" />
@@ -83,26 +83,26 @@ const Handbook = () => {
         Apartments (Ph: 951-683-3650), University Village Apartments (Ph:
         951-684-6828), Highlander Park Apartments (Ph: 951-683-1422) and
         Windwood Apartments (Ph: 951-787-6263).{" "}
-        <a
+        <Link
           href="https://housing.ucr.edu/"
           rel="noopener noreferrer"
           target="_blank"
           className="font-bold text-isa-blue-200 underline dark:text-isa-blue-200 hover:no-underline"
         >
           On-campus apartments
-        </a>{" "}
+        </Link>{" "}
         (Bannockburn Village & Plaza, Stonehaven and Family Housing) are also
         preferred by many. You can look up other apartments in the area by
         searching &quot;92507&quot; as the ZIP code, on any apartment rentals
         website (like{" "}
-        <a
+        <Link
           href="http://www.forrent.com"
           rel="noopener noreferrer"
           target="_blank"
           className="font-bold text-isa-blue-200 underline dark:text-isa-blue-200 hover:no-underline"
         >
           http://www.forrent.com
-        </a>
+        </Link>
         , which has an excellent listing for the UCR surronding area ZIP 92507).
         The rent for a 1BHK (bedroom-hall-kitchen) apartment is usually between
         $800-1000, while that for a 2BHK apartment is between $1000-1300. There
@@ -121,47 +121,39 @@ const Handbook = () => {
             Copies of all important documents (passport, visa, I-20), along with
             the originals obviously.
           </li>
-
           <li>
             Driving license from home state or international permit, if you plan
             to drive rental cars within a few months of arriving. $500-1000 in
             cash, $2000-3000 as TCs (in case you&apos;re not funded, you might
             have to bring a lot more).
           </li>
-
           <li>
             Books (contact the appropriate representative from the list at the
             bottom of this page; if one is not available for you major, try
             contacting a professor in your department).
           </li>
-
           <li>
             Stationery (pens, notebooks etc.) enough to last for about a month.
           </li>
-
           <li>
             Creams (moisturizer especially - it&apos;s dry here, sunscreen
             etc.), toothpaste, toothbrush etc. (all your beauty and health care
             supplies) enough to last for about a month.
           </li>
-
           <li>
             Basic medicines like Crocin, Erithromycin, Multivitamins and
             Aspirin, balms and any special eye or dental care supply you know
             you&apos;d need, with prescription.
           </li>
-
           <li>
             Plates, bowls, spoons, forks, knives, pressure cooker (one or two
             depending on what and how much you&apos;re planning to cook) etc.
           </li>
-
           <li>
             Masalas, pickles etc. that you might want for your food, enough to
             last for a couple of months or so (yes, everything is available here
             :)).
           </li>
-
           <li>
             Summer clothes, a couple of jackets or sweaters for winter, a couple
             of formal suits, few Indian clothes (to wear at those ISA parties to
@@ -173,22 +165,16 @@ const Handbook = () => {
         <Subtitle text="What to Do After Arriving?" />
         <ul class="pl-5 list-disc list-outside">
           <li>Open a bank account (Bank of America, Credit Union etc.).</li>
-
           <li>Finalize apartment, pay deposit and first month&apos;s rent.</li>
-
           <li>
             Take care of all the apartment utilities (especially electricity).
           </li>
-
           <li>Buy furniture, other room supplies.</li>
-
           <li>
             Apply for a social security number (SSN) (if you&apos;re going to be
             working on-campus as TA/GSR).
           </li>
-
           <li>Apply for a credit card (once you get the SSN).</li>
-
           <li>Apply for a state ID/driving license.</li>
         </ul>
       </div>
