73fd81bb9e9db08ebd4d9f03f31901952379bfed
raulrazo <129914276+raulrazo@users.noreply.github.com>
2024-01-26 14:44:14 -0800

    Update src/components/home/Values.jsx
    Co-authored-by: Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>

--- src/components/home/Values.jsx
+++ src/components/home/Values.jsx
@@ -6,7 +6,7 @@ import { VALUES } from "../../data/home/Values";
 const Values = () => {
   return (
     <div>
-      <Title text={"Our Core Values"} />
+      <Title text="Our Core Values" />
       <div className="flex flex-row items-center justify-center space-x-6 p-4 mt-6 mb-20">
         {VALUES.map((value, index) => (
           <Value
