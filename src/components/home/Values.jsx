b41373550b5ecd47f75c8af990abef50ebd2de6b
Raul Razo <raulrazo@Rauls-MacBook-Air-6.local>
2024-01-26 14:23:29 -0800

    Added title component on top values

--- src/components/home/Values.jsx
+++ src/components/home/Values.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import Value from "./Value";
+import Title from "../../components/Title.jsx";
 import { VALUES } from "../../data/home/Values";
 
 const Values = () => {
   return (
-    <div className="flex flex-row items-center justify-center space-x-6 p-4">
-      {VALUES.map((value, index) => (
-        <Value
-          key={index}
-          title={value.title}
-          icon={value.icon}
-          description={value.description}
-        />
-      ))}
+    <div>
+      <Title text={"Our Core Values"} />
+      <div className="flex flex-row items-center justify-center space-x-6 p-4 mt-6 mb-20">
+        {VALUES.map((value, index) => (
+          <Value
+            key={index}
+            title={value.title}
+            icon={value.icon}
+            description={value.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
