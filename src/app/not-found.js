ac16f1eb0f263b8588b28b5cb6087448acfd01b9
Sahana Anand <sahanaanand0516@gmail.com>
2024-01-29 02:03:49 -0800

    404 page changes

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,9 +1,9 @@
-import Subtitle from "@/components/handbook/Subtitle";
+import Title from "@/components/handbook/Title";
 import Button from "@/components/Button";
 const NotFound = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Subtitle text="404" />
+    <div className="flex flex-col items-center justify-center h-screen overscroll-none">
+      <Title text="404" />
       <p className="text-3xl font-bold bg-gradient-to-r from-isa-blue-200 to-isa-blue-100 text-transparent bg-clip-text mb-3">
         Page Not Found
       </p>
--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,11 @@ import About from "@/components/home/About";
 import What from "@/components/home/What";
 import Values from "@/components/home/Values";
 import Involved from "@/components/Involved";
+import NotFound from "./not-found";
 const Home = () => {
   return (
     <>
+      <NotFound />
       <Landing />
       <About />
       <What />
