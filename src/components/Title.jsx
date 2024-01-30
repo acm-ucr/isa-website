2d1102088b690511de8ad356abcb8a72e146b9ef
Raul Razo <raulrazo@ucr-secure-45-10-13-229-128.wnet.ucr.edu>
2024-01-29 16:38:45 -0800

    Added Title mobile responsiveness and fixed typo

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,14 +4,14 @@ import { PiStarFourFill } from "react-icons/pi";
 const Title = ({ text }) => {
   return (
     <div className="flex-col flex items-center">
-      <div className="text-isa-yellow-200 text-center font-extrabold text-4xl">
+      <div className="text-isa-yellow-200 text-center font-extrabold md:text-4xl text-3xl">
         {text}
       </div>
 
       <div className="flex items-center mt-3">
-        <div className="w-14 h-2 bg-isa-yellow-100 rounded mr-5" />
-        <PiStarFourFill className="w-9 h-9 text-isa-yellow-100 mr-5" />
-        <div className="w-14 h-2 bg-isa-yellow-100 rounded" />
+        <div className="md:w-14 w-11 md:h-2 h-2 bg-isa-yellow-100 rounded md:mr-5 mr-3" />
+        <PiStarFourFill className="md:w-9 w-7 md:h-9 h-7 text-isa-yellow-100 md:mr-5 mr-3" />
+        <div className="md:w-14 w-11 md:h-2 h-2 bg-isa-yellow-100 rounded" />
       </div>
     </div>
   );
