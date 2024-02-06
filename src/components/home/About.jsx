dd8ebf4f81a77b366fc049b8202cb91a6f69f1c6
Byron Bhuiyan <rafidb8@gmail.com>
2024-02-05 21:56:30 -0800

    minor change

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
     <div className="flex flex-col items-center my-4">
       <div className="flex flex-col sm:flex-row items-center justify-center px-10">
         <div className="flex justify-center items-center w-full sm:w-2/5">
-          <Image src={logo} alt="Picture of logo" className="z-10" />{" "}
+          <Image src={logo} alt="Picture of logo" className="z-10" />
         </div>
 
         <div className="flex flex-col items-center text-center w-full sm:w-3/5 mt-5 sm:mt-0 text-2xl">
