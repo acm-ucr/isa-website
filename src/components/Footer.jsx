cffbe20024348101b9374efb70e05494a22229ff
Marco Navarro <duvalincus@gmail.com>
2024-02-10 13:32:40 -0800

    larger footer and animated icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,16 @@ import logo from "../../public/images/navlogo.webp";
 const Footer = () => {
   return (
     <div className="bg-gradient-to-r from-isa-blue-200 to-isa-blue-100 w-full p-2 relative">
-      <div className="flex items-center font-bold text-3xl text-white space-x-3">
-        <Image src={logo} alt="ISA icon" className="h-[45px] w-[45px]" />
+      <div className="flex items-center font-bold text-3xl text-white space-x-3 h-[60px]">
+        <Image src={logo} alt="ISA icon" className="h-[50px] w-[50px]" />
         <div className="whitespace-nowrap">UCR ISA</div>
-        <div className="w-full max-h-full flex space-x-4 place-content-end">
+        <div className="w-full flex place-content-end align-items-center">
           {ICONS.map((icon, index) => (
-            <Link key={index} href={icon.link}>
+            <Link
+              key={index}
+              href={icon.link}
+              className={"hover:scale-110 duration-300 m-1.5"}
+            >
               {icon.logo}
             </Link>
           ))}
