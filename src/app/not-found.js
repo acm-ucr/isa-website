fefcb9046f7a1c25cf56ce8eb84be1fe53431493
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2024-02-10 16:12:24 -0800

    Update src/app/not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,7 +4,7 @@ import Button from "@/components/Button";
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen overscroll-none">
-      <Title text="404" className="text-7xl md:text-7xl" />
+      <Title text="404" size="text-7xl md:text-7xl" />
 
       <p className="text-3xl font-bold bg-gradient-to-r from-isa-blue-200 to-isa-blue-100 text-transparent bg-clip-text mb-3">
         Page Not Found
