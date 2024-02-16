ec212dd1c5dc4b8ca4931bdcb180837ab8c4336e
achen409 <andrewr.chen@gmail.com>
2024-02-16 14:48:53 -0800

    3 columns

--- src/components/home/Involved.jsx
+++ src/components/home/Involved.jsx
@@ -6,7 +6,7 @@ const Involved = () => {
   return (
     <div>
       <Title text="Get Involved" />
-      <div className="grid md:flex md:flex-row gap-y-5 grid-cols-2 justify-content-center pb-40 pt-12">
+      <div className="grid xl:flex xl:flex-row gap-y-5 grid-cols-2 md:grid-cols-3 justify-content-center pb-40 pt-12">
         {SOCIALS.map((social, index) => {
           return (
             <Social
