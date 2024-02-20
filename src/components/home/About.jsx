4597878951a13abd349f9ff26a7c7c03089575ae
ssahana05 <sahanaanand0516@gmail.com>
2024-02-20 13:46:11 -0800

    framer motion for about

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Title from "@/components/Title";
 
@@ -5,9 +6,19 @@ import Image from "next/image";
 import cloud from "@/public/assets/cloud.svg";
 import logo from "@/public/assets/logo.svg";
 import { motion } from "framer-motion";
-("use cilent");
 
-const animations = {
+const animationsLTR = {
+  start: {
+    opacity: 0,
+    x: -30,
+  },
+  end: {
+    opacity: 1,
+    x: 0,
+  },
+};
+
+const animationsDTU = {
   start: {
     opacity: 0,
     y: 30,
@@ -24,7 +35,7 @@ const About = () => {
       <div className="flex flex-col md:flex-row items-center justify-center px-10 lg:w-5/6">
         <div className="flex justify-center items-center w-full md:w-2/5">
           <motion.div
-            variants={animations}
+            variants={animationsLTR}
             initial="start"
             whileInView="end"
             transition={{ delay: 0.2 }}
@@ -34,13 +45,28 @@ const About = () => {
         </div>
 
         <div className="flex flex-col items-center text-center w-full md:w-3/5 mt-5 mb-10 md:mt-0 md:text-2xl text-lg">
-          <Title text="About us" />
+          <motion.div
+            variants={animationsDTU}
+            initial="start"
+            whileInView="end"
+            transition={{ delay: 0.2 }}
+          >
+            <Title text="About us" />
+          </motion.div>
           <div className="mt-4">
-            Indian Student Association-ISA at UCR serves the purpose to provide
-            a commonplace for people to share and exchange Indian culture of all
-            regions in India. We strive to provide a place for all Indians and
-            non-Indians to come to socialize, celebrate festivals, enjoy food,
-            meet new people, and learn more about Indian culture.
+            <motion.div
+              variants={animationsDTU}
+              initial="start"
+              whileInView="end"
+              transition={{ delay: 0.2 }}
+            >
+              Indian Student Association-ISA at UCR serves the purpose to
+              provide a commonplace for people to share and exchange Indian
+              culture of all regions in India. We strive to provide a place for
+              all Indians and non-Indians to come to socialize, celebrate
+              festivals, enjoy food, meet new people, and learn more about
+              Indian culture.
+            </motion.div>
           </div>
         </div>
       </div>
