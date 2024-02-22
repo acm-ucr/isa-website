262f1baeda246b1f167fb88ec037f1b1cb9ed0d2
achen409 <andrewr.chen@gmail.com>
2024-02-22 15:38:00 -0800

    added margin

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-col md:flex-row items-center justify-center px-10 lg:w-5/6">
-        <div className="flex justify-center items-center w-full md:w-2/5">
+        <div className="flex justify-center items-center w-full md:w-2/5 mr-10">
           <Image src={logo} alt="Picture of logo" className="z-10" />
         </div>
 
