175aedf397975ad23f259007fc2497981e807aad
achen409 <andrewr.chen@gmail.com>
2024-02-23 14:45:02 -0800

    changes 2

--- src/components/events/Calendar.jsx
+++ src/components/events/Calendar.jsx
@@ -19,7 +19,7 @@ const CalendarEvents = () => {
 
   useEffect(() => {
     const startDate = new Date(
-      new Date().getTime() - 60 * 60 * 24 * 7 * 10 * 1000
+      new Date().getTime() - 60 * 60 * 24 * 7 * 10 * 100000
     ).toISOString();
     const endDate = new Date(
       new Date().getTime() + 60 * 60 * 24 * 7 * 10 * 1000
