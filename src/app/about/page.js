e3b2730bd49c338fa3394c1214e257af2750dd93
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-02-25 15:56:28 -0800

    formatting

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -61,7 +61,7 @@ const About = () => {
           transition={{ ease: "easeOut", duration: 2, delay: 0.2 }}
           className="hidden lg:block absolute top-[500px] 2xl:-left-[13%] -left-[25%] scale-140"
         >
-         <Image src={Sun} alt="Blue sun" />
+          <Image src={Sun} alt="Blue sun" />
         </motion.div>
       </div>
 
