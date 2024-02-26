103365a76c318138e2d4ca563eda14fba1a478db
Byron Bhuiyan <rafidb8@gmail.com>
2024-02-26 03:55:03 -0800

    Added values animation

--- src/components/home/Values.jsx
+++ src/components/home/Values.jsx
@@ -1,13 +1,39 @@
+"use client";
 import React from "react";
 import Value from "./Value";
 import Title from "../../components/Title.jsx";
 import { VALUES } from "../../data/home/Values";
-
+import { motion } from "framer-motion";
+const animations = {
+  start: {
+    opacity: 0,
+    y: 30,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 const Values = () => {
   return (
-    <div className="mt-[5%]">
-      <Title text="Our Core Values" />
-      <div className="flex lg:flex-row flex-col items-center justify-center lg:space-y-0 space-y-10 lg:space-x-6 p-4 mt-6 mb-20">
+    <div className="relative flex flex-col items-center justify-center mt-[5%]">
+      <motion.div
+        variants={animations}
+        initial="start"
+        whileInView="end"
+        transition={{ delay: 0.2 }}
+        className="z-10"
+      >
+        <Title text="Our Core Values" />
+      </motion.div>
+
+      <motion.div
+        variants={animations}
+        initial="start"
+        whileInView="end"
+        transition={{ delay: 0.4 }} // Delay for the values to start after the title
+        className="flex lg:flex-row flex-col items-center justify-center lg:space-y-0 space-y-10 lg:space-x-6 p-4 mt-6 mb-20"
+      >
         {VALUES.map((value, index) => (
           <Value
             key={index}
@@ -16,7 +42,7 @@ const Values = () => {
             description={value.description}
           />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
