0a0a6607db4226c86cec51951ec3ec185784a054
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2024-03-29 22:22:48 -0700

    fixed board responsiveness

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -1,8 +1,8 @@
 const Board = ({ name, position }) => {
   return (
-    <div className="board flex">
-      <div className="h-h20 w-1.5 bg-gradient-to-t from-isa-yellow-200 to-isa-yellow-100 rounded-lg mr-2" />
-      <div className="flex-column align-center">
+    <div className="board flex md:text-left md:justify-start justify-center">
+      <div className="h-h20 w-1.5 bg-gradient-to-t from-isa-yellow-200 to-isa-yellow-100 rounded-lg mr-2 hidden md:block" />
+      <div className="flex flex-col items-center md:items-start">
         <p className="text-xl font-bold bg-gradient-to-r from-isa-yellow-200 to-isa-yellow-100 text-transparent bg-clip-text">
           {position}
         </p>
